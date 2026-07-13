d0912fc4e34f410ba72814c9a7f867de98e61166
KapszulasKaresz <nemethibence03@gmail.com>
2026-07-14 00:03:43 +0200

    correct vertex input for dx

--- libs/renderer/material/src/material_dx.cpp
+++ libs/renderer/material/src/material_dx.cpp
@@ -393,23 +393,23 @@ std::array<D3D12_INPUT_ELEMENT_DESC, 5> MaterialDX::getInputElementDescription()
     std::array<D3D12_INPUT_ELEMENT_DESC, 5> l_desc = {
         D3D12_INPUT_ELEMENT_DESC{ "POSITION",
                                  0,    DXGI_FORMAT_R32G32B32_FLOAT,
-                                 0,  0,
+                                 0, offsetof(mesh::Vertex, m_position),
                                  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
         D3D12_INPUT_ELEMENT_DESC{   "NORMAL",
                                  0,    DXGI_FORMAT_R32G32B32_FLOAT,
-                                 0, 12,
+                                 0, offsetof(mesh::Vertex,   m_normal),
                                  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
         D3D12_INPUT_ELEMENT_DESC{  "TANGENT",
                                  0, DXGI_FORMAT_R32G32B32A32_FLOAT,
-                                 0, 24,
+                                 0, offsetof(mesh::Vertex,  m_tangent),
                                  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
         D3D12_INPUT_ELEMENT_DESC{ "TEXCOORD",
                                  0,       DXGI_FORMAT_R32G32_FLOAT,
-                                 0, 40,
+                                 0, offsetof(mesh::Vertex, m_texCoord),
                                  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
         D3D12_INPUT_ELEMENT_DESC{    "COLOR",
                                  0,    DXGI_FORMAT_R32G32B32_FLOAT,
-                                 0, 48,
+                                 0, offsetof(mesh::Vertex,    m_color),
                                  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
     };
     return l_desc;
